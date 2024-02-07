1b63281d5f04f1e47b65ccfdd1de48db04c54e17
abhir <abhiramashok1062004@gmail.com>
2024-02-07 12:12:31 +0530

    Initial Commit

---
+++ Cycle3/ex18.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int n,i,j;
+    char str[20][20],temp[20];
+    printf("Enter the Number of Students: ");
+    scanf("%d",&n);
+    printf("Enter the name of %d Students\n",n);
+    for(i=0;i<n;i++){
+        scanf(" %[^\n]",str[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n-i-1;j++) {
+            if(strcasecmp(str[j],str[j+1])>0){
+                strcpy(temp,str[j]);
+                strcpy(str[j],str[j+1]);
+                strcpy(str[j+1],temp);
+            }
+        }
+    }
+    printf("Sorted List of Students are:\n");
+    for(i=0;i<n;i++){
+        printf("%s\n",str[i]);
+    }
+    return 0;
+}
