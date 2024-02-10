cdc9fbaa0c39bd17e92773a1f25f9ea39d5bcf26
abhir <abhiramashok1062004@gmail.com>
2024-02-10 13:14:00 +0530

    Initial Commit

--- Cycle4/ex20.c
+++ Cycle4/ex20.c
@@ -9,6 +9,7 @@ int factorial(int n) {
 double Sine(double x){
     double result=0;
     for(int n=0;n<9;n++){
+        //Using series expansion
         result+= (pow(-1,n)*pow(x,(2*n)+1))/factorial((2*n)+1);
     }
     return result;
